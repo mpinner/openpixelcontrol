28f9f352574df48d22c28b4c9b5de07cd3b8f372
mpinner <mpinner@gmail.com>
2014-07-15 11:17:01 -0700

    added greyscaling and 12-bit respective hack

--- src/tlc5947_server.c
+++ src/tlc5947_server.c
@@ -3,57 +3,42 @@
 
 #define POST_TX_DELAY_USECS 1000
 // I'm seeing flickering at speeds above ~6mhz. Although the 
-// TLC5947 datasheet lists a max rate of 25mhz, anecdotal evidence 
+// WS2801 datasheet lists a max rate of 25mhz, anecdotal evidence 
 // from forum posts suggests that most people have had trouble 
 // obtaining that in practice.
-#define TLC5947_DEFAULT_SPEED 4000000
+#define WS2801_DEFAULT_SPEED 4000000
 
-// Different TLC5947 strips expect different input color orderings.
+// Different WS2801 strips expect different input color orderings.
 typedef enum { RGB=0, GRB=1, BGR=2 } order_t;
 #define DEFAULT_INPUT_ORDER RGB
 
 static int spi_fd = -1;
 static u8 spi_data_tx[1 << 16];
-static u32 spi_speed_hz = TLC5947_DEFAULT_SPEED;
+static u32 spi_speed_hz = WS2801_DEFAULT_SPEED;
 static order_t rgb_order = DEFAULT_INPUT_ORDER;
 
-void tlC5947_put_pixels(int fd, u8 spi_data_tx[], u32 spi_speed_hz, 
+void ws2801_put_pixels(int fd, u8 spi_data_tx[], u32 spi_speed_hz, 
                        u16 count, pixel* pixels, order_t order) {
   int i;
   pixel* p;
   u8* d;
 
-  u8 zero = 0;
-
-  u8 brightness;
+  u16 lower = 0;
   
   d = spi_data_tx;
   for (i = 0, p = pixels; i < count; i++, p++) {
 
-    switch (order)
-    {
-    case RGB:
-      *d++ = p->r;
-      *d++ = zero;
-      *d++ = zero;
-      break;
-    case GRB:
-      *d++ = p->g;
-      *d++ = zero;
-      *d++ = zero;
-      break;
-    case BGR:
-      *d++ = p->b;
-      *d++ = zero;
-      *d++ = zero;
-      break;
-   
-
-    // here we do a brightness calc
-    // and transcode our colorspace to 12-bits
+    u16 fullPixel = (p->r+p->r+p->r + p->g+p->g+p->g+p->g + p->b ) << 1;    
+    // full shoul dnow be in the 12-bit space
     
-   // *d++ = (p->r + p->g + p->b ) / 2;
-   
+    if (0 == (i % 2)) {
+      *d++ = fullPixel >> 4;
+      lower = fullPixel;
+    } else {
+      
+      *d++ = fullPixel >> 8;
+      *d++ = fullPixel & 0xF;
+    }
   }
   spi_transfer(fd, spi_speed_hz, spi_data_tx, 0, 
       d - spi_data_tx, POST_TX_DELAY_USECS);
@@ -63,7 +48,7 @@ void tlC5947_put_pixels(int fd, u8 spi_data_tx[], u32 spi_speed_hz,
 void handler(u8 address, u16 count, pixel* pixels) {
   fprintf(stderr, "%d ", count);
   fflush(stderr);
-  tlC5947_put_pixels(spi_fd, spi_data_tx, spi_speed_hz, count, 
+  ws2801_put_pixels(spi_fd, spi_data_tx, spi_speed_hz, count, 
     pixels, rgb_order);
 }
 
@@ -88,13 +73,14 @@ int main(int argc, char** argv) {
 
   get_speed_and_port(&spi_speed_hz, &port, argc, argv);
   rgb_order = get_order(argc, argv);
-  spi_fd = init_spidev("/dev/spidev1.0", spi_speed_hz);
+  spi_speed_hz = 4000000;
+  spi_fd = init_spidev("/dev/spidev1.0", 4000000);
+
   if (spi_fd < 0) {
     return 1;
   }
   fprintf(stderr, "SPI speed: %.2f MHz, ready...\n", spi_speed_hz*1e-6);
   opc_source s = opc_new_source(port);
   while (s >= 0 && opc_receive(s, handler, TIMEOUT_MS));
-
   fprintf(stderr, "Exiting after %d ms of inactivity\n", TIMEOUT_MS);
 }
